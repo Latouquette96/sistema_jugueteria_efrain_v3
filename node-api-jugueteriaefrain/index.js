3bc7b776dc700a66850dd3e72f5afbd26e4f9127
David Emanuel Latouquette <54481812+Latouquette96@users.noreply.github.com>
2023-11-13 10:48:34 -0300

    API REST: Librerias incorporadas y cambios menores

--- node-api-jugueteriaefrain/index.js
+++ node-api-jugueteriaefrain/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Sincroniza la base de datos (puede eliminarla).
-db.sequelize.sync({force:false})
+db.sequelize.sync({force:true})
   .then(() => {
     console.log("Base de datos sincronizada.");
   })
